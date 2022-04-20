bc8b2870174984c6628d5f9ad7f8ae0d00d3eff8
Gamer Menace <hugo.holmberg@outlook.com>
2022-04-20 18:10:43 +0200

    Update compile_and_parse.py

--- compile_and_parse.py
+++ compile_and_parse.py
@@ -11,75 +11,132 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 ## Compiling data stuff for spreadsheet
 
-# What we're looking for and where + how many maps we've found
+# dicts of what we're looking for + how many matching names of those dicts we've found
 # Groundside maps
-icecount = 0
-bigred_count = 0
-lv624_count = 0
-magmoor_count = 0
-prison_count = 0
-whisk_count = 0
-chigusa_count = 0
-icycaves_count = 0
-icarus_count = 0
-orion_count = 0
+icecolony = {
+    "name": "Ice Colony",
+    "count": 0
+}
+bigred = {
+    "name": "Big Red",
+    "count": 0
+}
+v624 = {
+    "name": "LV624",
+    "count": 0
+}
+magmoor = {
+    "name": "Magmoor",
+    "count": 0
+}
+prisonstation = {
+    "name": "Prison Station",
+    "count": 0
+}
+whiskeyoutpost = {
+    "name": "Whiskey",
+    "count": 0
+}
+chigusa = {
+    "name": "Chigusa",
+    "count": 0
+}
+icycaves = {
+    "name": "Icy Caves",
+    "count": 0
+}
+icarus = {
+    "name": "Icarus",
+    "count": 0
+}
+orion = {
+    "name": "Orion",
+    "count": 0
+}
 # Ship maps
-theseus_count = 0
-minerva_count = 0
-sulaco_count = 0
-pillars_count = 0
+theseus = {
+    "name": "Theseus",
+    "count": 0
+}
+minerva = {
+    "name": "Minerva",
+    "count": 0
+}
+sulaco = {
+    "name": "Sulaco",
+    "count": 0
+}
+pillars = {
+    "name": "Pillars",
+    "count": 0
+}
 # Gamemode
-crash_count = 0
-distress_count = 0
-civwar_count = 0
-nuclearwar_count = 0
+crash = {
+    "name": "Crash",
+    "count": 0
+}
+distress = {
+    "name": "Distress",
+    "count": 0
+}
+civilwar = {
+    "name": "Civil War",
+    "count": 0
+}
+nuclearwar = {
+    "name": "Nuclear War",
+    "count": 0
+}
 # Amount of rounds
-roundscount = 0
+roundsamount = {
+    "name": "Round ID",
+    "count": 0
+}
 #The file
 datafile = "./[YEAR]/TGMC_[MONTH].txt" # NEW MONTH: Change to appropriate [YEAR]/TGMC_[MONTH].txt file
 read_data = open(datafile, encoding="utf-8")
 
-# Looking for stuff, probably terribly inefficient but hey you got-
+# Looking for stuff, probably terribly inefficient but hey i got-
 # - a good enough computer, so it doesn't matter, right?
 for line in read_data:
-    if "Ice Colony" in line:
-        icecount += 1
-    elif "Big Red" in line:
-        bigred_count += 1
-    elif "LV624" in line:
-        lv624_count += 1
-    elif "Magmoor" in line:
-        magmoor_count += 1
-    elif "Prison Station" in line:
-        prison_count += 1
-    elif "Whiskey Outpost" in line:
-        whisk_count += 1
-    elif "Chigusa" in line:
-        chigusa_count += 1
-    elif "Icy Caves" in line:
-        icycaves_count += 1
-    elif "Icarus" in line:
-        icarus_count += 1
-    elif "Orion Military Outpost" in line: # NEW MONTH: Hey is this still merged/TM'd/open?
-        orion_count += 1
-    if "Theseus" in line:
-        theseus_count += 1
-    elif "Minerva" in line:
-        minerva_count += 1
-    elif "Sulaco" in line:
-        sulaco_count += 1
-    elif "Pillar" in line:
-        pillars_count += 1
-    if "Crash" in line:
-        crash_count += 1
-    elif "Distress" in line:
-        distress_count += 1
-    elif "Nuclear War" in line:
-        nuclearwar_count += 1
-    elif "Civil War" in line:
-        civwar_count += 1
-    if "Round ID" in line:
-        roundscount += 1
+    if icecolony["name"] in line:
+        icecolony["count"] += 1
+    elif bigred["name"] in line:
+        bigred["count"] += 1
+    elif v624["name"] in line:
+        v624["count"] += 1
+    elif magmoor["name"] in line:
+        magmoor["count"] += 1
+    elif prisonstation["name"] in line:
+        prisonstation["count"] += 1
+    elif whiskeyoutpost["name"] in line:
+        whiskeyoutpost["name"] += 1
+    elif chigusa["name"] in line:
+        chigusa["count"] += 1
+    elif icycaves["name"] in line:
+        prisonstation["count"] += 1
+    elif icarus["name"] in line:
+        icarus["count"] += 1
+    elif orion["name"] in line: # NEW MONTH: Hey is this still merged/TM'd/open?
+        orion["count"] += 1
+    if theseus["name"] in line:
+        theseus["count"] += 1
+    elif minerva["name"] in line:
+        minerva["count"] += 1
+    elif sulaco["name"] in line:
+        sulaco["count"] += 1
+    elif pillars["name"] in line:
+        pillars["count"] += 1
+    if crash["name"] in line:
+        crash["count"] += 1
+    elif distress["name"] in line:
+        distress["count"] += 1
+    elif nuclearwar["name"] in line:
+        nuclearwar["count"] += 1
+    elif civilwar["name"] in line:
+        civilwar["count"] += 1
+    if roundsamount["name"] in line:
+        roundsamount["count"] += 1
 
 ## Spreadsheet stuff
 
@@ -89,32 +146,18 @@ creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope) #h
 client = gspread.authorize(creds)
 # Format i personally use is "TGMC_Datasheet_[Month]_[Year]"
 sheet = client.open("TGMC_Datasheet_[Month]_[Year]").sheet1 # NEW MONTH: Change to appropriate month/year sheet 
-#List of groundside, shipside maps, crash/distress rounds, and total amount of rounds + the amount we found
-BigRed = ["Big Red", bigred_count]
-IceCo = ["Ice Colony", icecount]
-V624 = ["LV624", lv624_count]
-Magmoor = ["Magmoor", magmoor_count]
-PrisonStation = ["Prison Stat.", prison_count]
-Whiskey = ["Whiskey-Post.", whisk_count]
-Chigusa = ["Chigusa", chigusa_count]
-IcyCaves = ["Icy Caves", icycaves_count]
-Icarus = ["Icarus", icarus_count]
-Orion = ["Orion M.O.", orion_count]
-Theseus = ["Theseus", theseus_count]
-Minerva = ["Minerva", minerva_count]
-Sulaco = ["Sulaco", sulaco_count]
-Pillars = ["PoS.", pillars_count]
-Crash = ["Crash", crash_count]
-Distress = ["Distress", distress_count]
-Civilwar = ["Civil War", civwar_count]
-Nuclearwar = ["Nuclear War",nuclearwar_count]
-Totalrounds = ["Total Rounds:", roundscount]
+
 #stuff to make things easier
 quicksleep = time.sleep(3) # just to not bother with typing time.sleep(3) everytime
 def shade(str):
     sheet.format(str, {"backgroundColor": {"red": 0.811,"green": 0.811,"blue": 0.811}})
 def altshade(str):
     sheet.format(str,{"backgroundColor": {"red": 0.380,"green": 0.380,"blue": 0.380},"textFormat": {"foregroundColor": {"red": 1.0,"green": 1.0,"blue": 0.0}}})
+def thingstats(dict,index):
+    x = dict["count"]
+    y = dict["name"]
+    z = [y,x]
+    sheet.insert_row(z,index)
 ## The spreadsheet code
 #Delete & unformat anything we previously had there
 sheet.delete_rows(1,24)
@@ -125,35 +168,36 @@ sheet.delete_rows(1,24)
 ###
 sheet.insert_row(['Post-round groundside map picks:'])
 sheet.update_cell(1,3, '% +/- difference')
-sheet.insert_row(BigRed,2)
-sheet.insert_row(IceCo,3)
+thingstats(bigred,2)
+thingstats(icecolony,3)
 quicksleep
-sheet.insert_row(V624,4)
-sheet.insert_row(Magmoor,5)
-sheet.insert_row(PrisonStation,6)
-sheet.insert_row(Chigusa,7)
+thingstats(v624,4)
+thingstats(magmoor,5)
+thingstats(prisonstation,6)
+thingstats(chigusa,7)
 quicksleep
-sheet.insert_row(IcyCaves,8)
-sheet.insert_row(Icarus,9)
-sheet.insert_row(Whiskey,10)
-sheet.insert_row(Orion,11)
+thingstats(icycaves,8)
+thingstats(icarus,9)
+thingstats(whiskeyoutpost,10)
+thingstats(orion,11)
 quicksleep
 sheet.insert_row(['Post-round shipside map picks:'],13)
 sheet.update_cell(13,3, '% +/- difference')
-sheet.insert_row(Theseus,14)
-sheet.insert_row(Minerva,15)
+thingstats(theseus,14)
+thingstats(minerva,15)
 quicksleep
-sheet.insert_row(Sulaco,16)
-sheet.insert_row(Pillars,17)
+thingstats(sulaco,16)
+thingstats(pillars,17)
 sheet.insert_row(['Round gamemodes:'],19)
 sheet.update_cell(19,3, '% +/- difference')
 quicksleep
-sheet.insert_row(Crash,20)
-sheet.insert_row(Distress,21)
-sheet.insert_row(Civilwar,22)
-sheet.insert_row(Nuclearwar,23)
+thingstats(crash,20)
+thingstats(distress,21)
+thingstats(civilwar,22)
+thingstats(nuclearwar,23)
 quicksleep
-sheet.insert_row(Totalrounds,24)
+thingstats(roundsamount,24)
+sheet.update_cell(10,7,'All data here was collected between:')
 sheet.update_cell(10,7,'All data here was collected between:')
 sheet.update_cell(11,7,'[MONTH] 1st and [MONTH] [END OF MONTH DAY], year [YEAR].') # NEW MONTH: Update month (& Year if needed)
 sheet.format(
