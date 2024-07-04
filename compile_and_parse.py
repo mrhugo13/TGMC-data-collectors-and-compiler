59c7240cf0f578141c60b21a755b2a41c01e3102
Gamer Menace <hugo.holmberg@outlook.com>
2024-07-04 16:10:29 +0200

    Update compile_and_parse.py

--- compile_and_parse.py
+++ compile_and_parse.py
@@ -5,18 +5,26 @@ from openpyxl import Workbook
 from datetime import datetime
 
 ## This is to determine the month so we don't have to do as much maintenance.
-
-manualorautomatic = input("Would you like to input month/year manually (Y) or have it done automatically for todays year/month? (N): ")
+manualorautomatic = int(input("Would you like to input month/year manually (1)\nor have it done automatically for todays year/month? (2)\nor have it done automatically for yesterdays year/month (3): "))
 manualorautomaticpicked = NULL
 match manualorautomatic:
-    case "Y":
+    case 1:
         print("You have picked to input month/year manually.")
         manualorautomaticpicked = True
-    case "N":
-        print("You have picked to have the month/year part automated")
+    case 2:
+        print("You have picked to have the month/year part automated for todays year/month")
         manualorautomaticpicked = False
         currentMonth = datetime.now().month
         currentYear = datetime.now().year
+    case 3:
+        print("You have picked to have the month/year part automated for yesterdays year/month")
+        manualorautomaticpicked = False
+        currentMonth = datetime.now().month - 1
+        if currentMonth == 0:
+            currentYear = datetime.now() - 1
+            currentMonth = 12
+        else:
+            currentYear = datetime.now().year
     case _:
         print("Invalid input - Exitting program.")
         exit()
@@ -26,155 +34,102 @@ if(manualorautomaticpicked):
     currentYear = int(input("Pick the year desired: "))
 
 themonth = "TGMC_"
+lastmonth = "TGMC_"
 match currentMonth:
     case 0:
         themonth += "December"
+        lastmonth += "November"
     case 1:
         themonth += "January"
+        lastmonth += "December"
     case 2:
         themonth += "February"
+        lastmonth += "January"
     case 3:
         themonth += "March"
+        lastmonth += "February"
     case 4:
         themonth += "April"
+        lastmonth += "March"
     case 5:
         themonth += "May"
+        lastmonth += "April"
     case 6:
         themonth += "June"
+        lastmonth += "May"
     case 7:
         themonth += "July"
+        lastmonth += "June"
     case 8:
         themonth += "August"
+        lastmonth += "July"
     case 9:
         themonth += "September"
+        lastmonth += "August"
     case 10:
         themonth += "October"
+        lastmonth += "September"
     case 11:
         themonth += "November"
+        lastmonth += "October"
     case 12:
         themonth += "December"
+        lastmonth += "November"
     case _:
         print("Invalid month picked - Exitting program.")
         exit()
-
+    
 # i really don't wanna maintain the month/year thingy
 file_name_txt = f"./{currentYear}/{themonth}.txt"
 file_name_txt_results = f"./{currentYear}_results/{themonth}.txt"
+file_name_txt_results_previous = f"./{currentYear}_results/{lastmonth}.txt"
 file_name_xlsx = f"./{currentYear}/{themonth}.xlsx"
 file_name_xlsx_results = f"./{currentYear}_results/{themonth}.xlsx"
 
 ## Compiling data stuff for spreadsheet
+file_name_config = f"./config.txt"
 
-# dicts of what we're looking for + how many matching names of those dicts we've found
-# Groundside maps
-icecolony = {
-    "name": "Ice Colony",
-    "count": 0,
-    "percentage": 0
-}
-bigred = {
-    "name": "Big Red",
-    "count": 0,
-    "percentage": 0
-}
-v624 = {
-    "name": "LV624",
-    "count": 0,
-    "percentage": 0
-}
-magmoor = {
-    "name": "Magmoor Digsite IV",
-    "count": 0,
-    "percentage": 0
-}
-prisonstation = {
-    "name": "Prison Station",
-    "count": 0,
-    "percentage": 0
-}
-whiskeyoutpost = {
-    "name": "Whiskey Outpost",
-    "count": 0,
-    "percentage": 0
-}
-chigusa = {
-    "name": "Chigusa",
-    "count": 0,
-    "percentage": 0
-}
-icycaves = {
-    "name": "Icy Caves",
-    "count": 0,
-    "percentage": 0
-}
-icarus = {
-    "name": "Icarus Military Port",
-    "count": 0,
-    "percentage": 0
-}
-orion = {
-    "name": "Orion Military Outpost",
-    "count": 0,
-    "percentage": 0
-}
-polarcolony = {
-    "name": "Rocinante Polar Colony",
-    "count": 0,
-    "percentage": 0
-}
-researchoutpost = {
-    "name": "Research Outpost",
-    "count": 0,
-    "percentage": 0
-}
-# Ship maps
-theseus = {
-    "name": "Theseus",
-    "count": 0,
-    "percentage": 0
-}
-minerva = {
-    "name": "Minerva",
-    "count": 0,
-    "percentage": 0
-}
-sulaco = {
-    "name": "Sulaco",
-    "count": 0,
-    "percentage": 0
-}
-pillars = {
-    "name": "Pillar of Spring",
-    "count": 0,
-    "percentage": 0
-}
-# Gamemode
-crash = {
-    "name": "Crash",
-    "count": 0,
-    "percentage": 0
-}
-distress = {
-    "name": "Distress",
-    "count": 0,
-    "percentage": 0
-}
-civilwar = {
-    "name": "Civil War",
-    "count": 0,
-    "percentage": 0
-}
-nuclearwar = {
-    "name": "Nuclear War",
-    "count": 0,
-    "percentage": 0
-}
-# Amount of rounds
-roundsamount = {
-    "name": "Round ID",
-    "count": 0,
-    "percentage": "N/A" #this is irrelevant
-}
+groundmaps = []
+groundmapdicts = []
+shipmaps = []
+shipmapdicts = []
+gamemodes = []
+gamemodedicts = []
+rounds = []
+rounddicts = []
+AllLists = groundmaps, shipmaps, gamemodes, rounds
+mode = groundmaps
+
+configfile = file_name_config
+read_config = open(configfile, encoding="utf-8")
+
+for line in read_config:
+    if "GroundMaps" in line:
+        mode = groundmaps
+    elif "ShipMaps" in line:
+        mode = shipmaps
+    elif "Gamemodes" in line:
+        mode = gamemodes
+    elif "Rounds" in line:
+        mode = rounds
+    else:
+        mode += [line.strip("\n")]
+
+def CreateDictsOf(list, list2):
+    for i in list:
+        a = {}
+        a[i] = {
+            "name": i,
+            "count": 0,
+            "percentage": 0,
+            "increase_decrease": 0,
+        }
+        list2.append(a)
+
+CreateDictsOf(groundmaps,groundmapdicts)
+CreateDictsOf(shipmaps,shipmapdicts)
+CreateDictsOf(gamemodes,gamemodedicts)
+CreateDictsOf(rounds,rounddicts)
 # total count of ships/groundmaps/gamemodes so we can automate percentages
 groundmapstotalcount = 0
 shipmapstotalcount = 0
@@ -186,140 +141,75 @@ read_data = open(datafile, encoding="utf-8")
 # Looking for stuff, probably terribly inefficient but hey i got-
 # - a good enough computer, so it doesn't matter, right?
 for line in read_data:
-    if icecolony["name"] in line:
-        icecolony["count"] += 1
-        groundmapstotalcount += 1
-    elif bigred["name"] in line:
-        bigred["count"] += 1
-        groundmapstotalcount += 1
-    elif v624["name"] in line:
-        v624["count"] += 1
-        groundmapstotalcount += 1
-    elif magmoor["name"] in line:
-        magmoor["count"] += 1
-        groundmapstotalcount += 1
-    elif prisonstation["name"] in line:
-        prisonstation["count"] += 1
-        groundmapstotalcount += 1
-    elif whiskeyoutpost["name"] in line:
-        whiskeyoutpost["count"] += 1
-        groundmapstotalcount += 1
-    elif chigusa["name"] in line:
-        chigusa["count"] += 1
-        groundmapstotalcount += 1
-    elif icycaves["name"] in line:
-        icycaves["count"] += 1
-        groundmapstotalcount += 1
-    elif icarus["name"] in line:
-        icarus["count"] += 1
-        groundmapstotalcount += 1
-    elif orion["name"] in line:
-        orion["count"] += 1
-        groundmapstotalcount += 1
-    elif polarcolony["name"] in line: # NEW MONTH: Hey is this still merged/TM'd/open?
-        polarcolony["count"] += 1
-        groundmapstotalcount += 1
-    elif researchoutpost["name"] in line:
-        researchoutpost["count"] += 1
-        groundmapstotalcount += 1
-    if theseus["name"] in line:
-        theseus["count"] += 1
-        shipmapstotalcount += 1
-    elif minerva["name"] in line:
-        minerva["count"] += 1
-        shipmapstotalcount += 1
-    elif sulaco["name"] in line:
-        sulaco["count"] += 1
-        shipmapstotalcount += 1
-    elif pillars["name"] in line:
-        pillars["count"] += 1
-        shipmapstotalcount += 1
-    if crash["name"] in line:
-        crash["count"] += 1
-        gamemodestotalcount += 1
-    elif distress["name"] in line:
-        distress["count"] += 1
-        gamemodestotalcount += 1
-    elif nuclearwar["name"] in line:
-        nuclearwar["count"] += 1
-        gamemodestotalcount += 1
-    elif civilwar["name"] in line:
-        civilwar["count"] += 1
-        gamemodestotalcount += 1
-    if roundsamount["name"] in line:
-        roundsamount["count"] += 1
+    for i in groundmaps:
+        if i in line:
+            groundmapdicts[groundmaps.index(i)][i]["count"] += 1
+            groundmapstotalcount += 1
+    for i in shipmaps:
+        if i in line:
+            shipmapdicts[shipmaps.index(i)][i]["count"] += 1
+            shipmapstotalcount += 1
+    for i in gamemodes:
+        if i in line:
+            gamemodedicts[gamemodes.index(i)][i]["count"] += 1
+            gamemodestotalcount += 1
+    for i in rounds:
+        if i in line:
+            rounddicts[rounds.index(i)][i]["count"] += 1
 
 ## Spreadsheet stuff
 #stuff to make things easier for us here
 wb = Workbook()
 ws = wb.active
-groundmaps = [icecolony,icycaves,bigred,v624,magmoor,whiskeyoutpost,researchoutpost,polarcolony,orion,icarus,chigusa,prisonstation]
-shipmaps = [theseus, minerva, sulaco, pillars]
-gamemodes = [crash, distress, nuclearwar, civilwar]
-def thingstats(dict):
-    w = dict["percentage"]
-    x = dict["count"]
-    y = dict["name"]
-    z = [y,x,w]
-    a = str(z)
-    pprint(z)
-    f = open(file_name_txt_results, "a")
-    f.write(f"{a}\n")
-    f = open(file_name_xlsx_results, "a")
-    ws.append(z)
-    wb.save(file_name_xlsx_results)
+def thingstats(list1,list2):
+    for i in list2:
+        v = list1[list2.index(i)][i]["increase_decrease"]
+        w = list1[list2.index(i)][i]["percentage"]
+        x = list1[list2.index(i)][i]["count"]
+        y = list1[list2.index(i)][i]["name"]
+        z = [y,x,w,v]
+        a = str(z)
+        pprint(z)
+        f = open(file_name_txt_results, "a")
+        f.write(f"{a}\n")
+        f = open(file_name_xlsx_results, "a")
+        ws.append(z)
+        wb.save(file_name_xlsx_results)
 def whitespace():
     ws.append([" "," "])
     wb.save(file_name_xlsx_results)
 def whatisit(str1, str2, str3, str4):
     ws.append([str1,str2, str3, str4])
     wb.save(file_name_xlsx_results)
-def percentagemathing(list, var):
-    for dict in list:
-        if (dict["count"] < 1):
-            dict["percentage"] = "N/A"
+def percentagemathing(list1,list2, var):
+    for i in list2:
+        if (list1[list2.index(i)][i]["count"] < 1):
+            list1[list2.index(i)][i]["percentage"] = "N/A"
         else:
-            dict["percentage"] = (dict["count"] / var) * 100
-            dict["percentage"] = round(dict["percentage"], 1)
-            dict["percentage"] = str(dict["percentage"])
-            dict["percentage"] += "%"
+            list1[list2.index(i)][i]["percentage"] = (list1[list2.index(i)][i]["count"] / var) * 100
+            list1[list2.index(i)][i]["percentage"] = round(list1[list2.index(i)][i]["percentage"], 1)
+            list1[list2.index(i)][i]["percentage"] = str(list1[list2.index(i)][i]["percentage"])
+            list1[list2.index(i)][i]["percentage"] += "%"
 ## The spreadsheet code
 #Delete & unformat anything we previously had there
 f = open(file_name_xlsx_results, "w")
 f = open(file_name_txt_results, "w")
 #counting percentages
-percentagemathing(groundmaps, groundmapstotalcount)
-percentagemathing(shipmaps, shipmapstotalcount)
-percentagemathing(gamemodes, gamemodestotalcount)
+percentagemathing(groundmapdicts,groundmaps, groundmapstotalcount)
+percentagemathing(shipmapdicts,shipmaps, shipmapstotalcount)
+percentagemathing(gamemodedicts,gamemodes, gamemodestotalcount)
 #New stuff
 whatisit("Post-round groundside map choices", "Times picked", "% picked","% increase/decrease")
-thingstats(bigred)
-thingstats(icecolony)
-thingstats(v624)
-thingstats(magmoor)
-thingstats(prisonstation)
-thingstats(chigusa)
-thingstats(icycaves)
-thingstats(icarus)
-thingstats(whiskeyoutpost)
-thingstats(orion)
-thingstats(polarcolony)
-thingstats(researchoutpost)
+thingstats(groundmapdicts,groundmaps)
 whitespace()
 whatisit("Post-round shipside map choices", "Times picked", "% picked","% increase/decrease")
-thingstats(theseus)
-thingstats(minerva)
-thingstats(sulaco)
-thingstats(pillars)
+thingstats(shipmapdicts,shipmaps)
 whitespace()
 whatisit("Gamemodes", "Times played", "% played","% Increase/decrease")
-thingstats(crash)
-thingstats(distress)
-thingstats(civilwar)
-thingstats(nuclearwar)
+thingstats(gamemodedicts,gamemodes)
 whitespace()
 whatisit("Rounds played:", " ", " ", " ")
-thingstats(roundsamount)
+thingstats(rounddicts,rounds)
 ## Style stuff for spreadsheet
 # this just adjusts columns to fit our data
 # stolen from velis @ https://stackoverflow.com/questions/13197574/openpyxl-adjust-column-width-size
